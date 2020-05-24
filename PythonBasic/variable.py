b555a85d02571af043a8e2862b1f323f641401a6
patilsa <sagar.patil@autodesk.com>
2020-05-24 20:59:25 +0530

    Adding variable file

---
+++ PythonBasic/variable.py
@@ -0,0 +1,27 @@
+a = 5
+print (a)
+
+b = "Hello"
+
+print (b)
+
+import turtle
+
+test_turtle = turtle.Turtle()
+
+def fuc_turtle():
+    #test_turtle = turtle.Turtle()
+    test_turtle.forward(100)
+    test_turtle.right(90)
+    test_turtle.forward(100)
+    test_turtle.right(90)
+    test_turtle.forward(100)
+    test_turtle.right(90)
+    test_turtle.forward(100)
+
+
+fuc_turtle()
+test_turtle.forward(110)
+fuc_turtle()
+test_turtle.forward(110)
+fuc_turtle()
